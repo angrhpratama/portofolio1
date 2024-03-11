23af38bd8ca13ab58150659a6182b56ac6ff20e4
angrhpratama <angrhpratama0@gmail.com>
2024-03-11 18:43:20 +0700

    size

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -2,12 +2,13 @@ import { Box, Center, Image } from "@chakra-ui/react"
 
 function Home() {
     return (
-        
+
         <Center>
-            <Box boxSize='flex'>
-                <Image 
-                src='https://res.cloudinary.com/dhioc6nup/image/upload/v1709955004/porto/vsqw8fuhyg96042x1mzt.jpg' alt='picture' />
-            </Box>
+                <Image
+                    src='https://res.cloudinary.com/dhioc6nup/image/upload/v1709955004/porto/vsqw8fuhyg96042x1mzt.jpg'
+                    alt='picture'
+                    height='700'
+                />
         </Center>
 
     )
